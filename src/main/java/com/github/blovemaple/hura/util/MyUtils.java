a86851944a196ee9b7c02b7d7cd06010bb218cb5
blovemaple <blovemaple2010@gmail.com>
2016-12-04 20:23:37 +0800

    改进判断汉字的逻辑

--- src/main/java/com/github/blovemaple/hura/util/MyUtils.java
+++ src/main/java/com/github/blovemaple/hura/util/MyUtils.java
@@ -10,13 +10,24 @@ package com.github.blovemaple.hura.util;
  */
 public class MyUtils {
 
+	/**
+	 * 判断指定字符串是否含有汉字。
+	 */
 	public static boolean hasChinese(String str) {
 		return str.chars().anyMatch(c -> isChinese((char) c));
 	}
 
-	public static boolean isChinese(char a) {
-		int v = (int) a;
-		return (v >= 19968 && v <= 171941);
+	/**
+	 * 判断指定字符是否是汉字。参考自：<br>
+	 * https://github.com/mabe02/lanterna/blob/62b0b57cd62cc5cf9cd4ff7035c067ba1356598d/src/main/java/com/googlecode/lanterna/TerminalTextUtils.java
+	 */
+	public static boolean isChinese(char c) {
+		Character.UnicodeBlock unicodeBlock = Character.UnicodeBlock.of(c);
+		return (unicodeBlock == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS)
+				|| (unicodeBlock == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A)
+				|| (unicodeBlock == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B)
+				|| (unicodeBlock == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS)
+				|| (unicodeBlock == Character.UnicodeBlock.CJK_RADICALS_SUPPLEMENT);
 	}
 
 }
