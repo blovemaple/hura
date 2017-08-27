b333ef87682e805ab49dfaa01154c8e2a5579975
blovemaple <blovemaple2010@gmail.com>
2017-08-28 00:18:33 +0800

    世汉词典过滤掉相关度低的结果

--- src/main/java/com/github/blovemaple/hura/source/ChenVortaro.java
+++ src/main/java/com/github/blovemaple/hura/source/ChenVortaro.java
@@ -10,10 +10,13 @@ import java.nio.file.Paths;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
@@ -31,7 +34,13 @@ import com.google.gson.Gson;
  * 
  * @author blovemaple <blovemaple2010(at)gmail.com>
  */
+/**
+ * @author blovemaple <blovemaple2010(at)gmail.com>
+ *
+ */
 public class ChenVortaro implements VortaroSource {
+
+	private boolean goodOnly = true;
 
 	private Gson gson = new Gson();
 
@@ -42,6 +51,13 @@ public class ChenVortaro implements VortaroSource {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public ChenVortaro() {
+	}
+
+	public ChenVortaro(boolean goodOnly) {
+		this.goodOnly = goodOnly;
 	}
 
 	@Override
@@ -67,12 +83,28 @@ public class ChenVortaro implements VortaroSource {
 
 			if (hasChinese(vorto)) {
 				// 输入是中文
-				// 取所有在非括号内出现关键词的结果
-				return queryResult.getList().stream()
+				List<ListItem> finalItems;
+
+				// 取有效结果
+				List<ListItem> validItems = queryResult.getList().stream()
 						// 清理一下
 						.peek(this::cleanSignifo)
-						// 在非括号内出现关键词的结果
-						.filter(item -> isValidSignifo(item.getSignifo(), vorto))
+						// 过滤出有效结果
+						.filter(item -> isValidSignifo(item.getSignifo(), vorto)).collect(Collectors.toList());
+
+				if (goodOnly) {
+					// 从有效结果中取好的结果
+					List<ListItem> goodItems = validItems.stream()
+							// 过滤出好的结果
+							.filter(item -> isGoodSignifo(item.getSignifo(), vorto)) //
+							.collect(Collectors.toList());
+					// 如果有好的结果，只取好的；否则取所有有效的
+					finalItems = goodItems.isEmpty() ? validItems : goodItems;
+				} else {
+					finalItems = validItems;
+				}
+
+				return finalItems.stream() //
 						.map(item -> new VortaroSourceResult(item.getRadiko(), item.getSignifo()))
 						.collect(Collectors.toList());
 			} else {
@@ -118,23 +150,63 @@ public class ChenVortaro implements VortaroSource {
 		item.setSignifo(item.getSignifo().replaceAll("\\/.+", "").replaceAll("参 考 资 料.*", "").trim());
 	}
 
+	/**
+	 * 除了“[...]”以外的部分出现查询词，才是有效的含义。
+	 */
 	private boolean isValidSignifo(String signifo, String vorto) {
-		String pureSignifo = signifo.replaceAll("\\[[^\\[\\]]+\\]", "");
-		while (!signifo.equals(pureSignifo)) {
-			signifo = pureSignifo;
-			pureSignifo = pureSignifo.replaceAll("\\[[^\\[\\]]+\\]", "");
-		}
+		String pureSignifo = getPureSignifo(signifo);
 		return pureSignifo.contains(vorto);
 	}
 
-	public static void main(String[] args) {
-		String signifo = "[火车]哈哈[ef[78787]joei]";
+	/**
+	 * 符合此条件是好的含义：<br>
+	 * 除了“[...]”以外的部分按照非中文字符分隔，单独出现查询词，或者单独出现查询词后加“的”或“地”。
+	 */
+	private boolean isGoodSignifo(String signifo, String vorto) {
+		List<String> words = splitWords(getPureSignifo(signifo));
+		List<String> targetWords = Arrays.asList(vorto, vorto + "的", vorto + "地");
+		return !Collections.disjoint(words, targetWords);
+	}
+
+	private String getPureSignifo(String signifo) {
 		String pureSignifo = signifo.replaceAll("\\[[^\\[\\]]+\\]", "");
 		while (!signifo.equals(pureSignifo)) {
 			signifo = pureSignifo;
 			pureSignifo = pureSignifo.replaceAll("\\[[^\\[\\]]+\\]", "");
 		}
-		System.out.println(pureSignifo);
+		return pureSignifo;
+	}
+
+	private List<String> splitWords(String signifo) {
+		if (StringUtils.isBlank(signifo))
+			return Collections.emptyList();
+
+		List<String> words = new ArrayList<>();
+		StringBuilder crtWord = new StringBuilder();
+		signifo.chars().forEach(c -> {
+			if (isChinese((char) c)) {
+				crtWord.append((char) c);
+			} else {
+				if (StringUtils.isNotBlank(crtWord)) {
+					words.add(crtWord.toString());
+				}
+				crtWord.setLength(0);
+			}
+		});
+		if (StringUtils.isNotBlank(crtWord)) {
+			words.add(crtWord.toString());
+		}
+		return words;
+	}
+
+	public static void main(String[] args) throws IOException {
+		String vorto = "重要";
+
+		ChenVortaro vortaro = new ChenVortaro();
+		ChenVortaro validVortaro = new ChenVortaro(false);
+		vortaro.query(vorto).forEach(System.out::println);
+		System.out.println();
+		validVortaro.query(vorto).forEach(System.out::println);
 	}
 
 }
