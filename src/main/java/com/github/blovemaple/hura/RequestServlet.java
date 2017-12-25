bfaf861240bc940dfe03e159b6009819662b0b95
blovemaple <blovemaple2010@gmail.com>
2017-12-26 01:17:41 +0800

    记录日志

--- src/main/java/com/github/blovemaple/hura/RequestServlet.java
+++ src/main/java/com/github/blovemaple/hura/RequestServlet.java
@@ -1,10 +1,13 @@
 package com.github.blovemaple.hura;
 
+import static com.github.blovemaple.hura.ResponseStatus.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,6 +15,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.pmw.tinylog.Logger;
 
 import com.github.blovemaple.hura.ocr.GoogleOcr;
 import com.github.blovemaple.hura.source.VortaroSource;
@@ -59,46 +64,47 @@ public class RequestServlet extends HttpServlet {
 		try {
 			message = parseMessage(request);
 			if (message == null) {
-				noResponse(response);
+				noResponse(null, response);
 				return;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			noResponse(response);
+			noResponse(null, response);
 			return;
 		}
+
+		long startTime = System.currentTimeMillis();
 
 		try {
 			switch (message.getMsgType()) {
 			case "event":
 				if ("subscribe".equals(message.getEvent())) {
-					writeResponse("Bonvenon!\n"
+					writeResponse(SUCC, startTime, "Bonvenon!\n"
 							+ "欢迎使用Hura！Hura是一个世界语汉语双向词典/翻译工具，向我（Hura公众号）发送世界语或汉语即可得到解释或翻译。目前Hura的词典来源为陈在伟老师提供的世汉词典，以及lernu.net词典作为辅助；若两个词典均查不到，则会使用谷歌翻译。\n"
 							+ "目前Hura还不成熟，如有改进建议请发送邮件给作者：blovemaple2010@gmail.com。\n"
 							+ "Hura后台服务代码开源，如果你是程序猿/媛朋友，可在Github上搜索“hura”。\n" + "希望Hura能帮到你。 :)", message, response);
 					return;
 				} else
-					noResponse(response);
+					noResponse(message, response);
 				return;
 			case "text":
 				String reqContent = message.getContent();
 				if (reqContent == null || reqContent.isEmpty()) {
-					noResponse(response);
+					noResponse(message, response);
 					return;
 				}
 				List<VortaroResult> results = vortaro.query(reqContent, QUERY_TIMEOUT);
 				if (results != null && !results.isEmpty())
-					writeResponse(resultsToString(results), message, response);
+					writeResponse(SUCC, startTime, resultsToString(results), message, response);
 				else
-					writeResponse("Mi ne komprenas ĉi tiun tekston.\n抱歉，我看不懂你输入的内容。 :(", message, response);
+					writeResponse(FAIL, startTime, "Mi ne komprenas ĉi tiun tekston.\n抱歉，我看不懂你输入的内容。 :(", message,
+							response);
 				return;
 			case "image":
 				if (myUserName.equals(message.getFromUserName())) {
-					long startTime=System.currentTimeMillis();
-					
 					String picUrl = message.getPicUrl();
 					if (picUrl == null || picUrl.isEmpty()) {
-						noResponse(response);
+						noResponse(message, response);
 						return;
 					}
 					String text;
@@ -106,12 +112,13 @@ public class RequestServlet extends HttpServlet {
 						text = ocr.recognize(picUrl);
 					} catch (IOException e) {
 						e.printStackTrace();
-						writeResponse("Mi bedaŭras, sistemeraro estas okazinta.\n抱歉，图片识别暂时出了点问题。请发文字吧。 :(", message,
+						writeResponse(SERR, startTime,
+								"Mi bedaŭras, sistemeraro estas okazinta.\n抱歉，图片识别暂时出了点问题。请发文字吧。 :(", message,
 								response);
 						return;
 					}
 					if (text == null || text.isEmpty()) {
-						writeResponse(
+						writeResponse(FAIL, startTime,
 								"Mi bedaŭras, estas ne teksto en ĉi tiu bildo.\n抱歉，我识别不出图片里的文字，或是您的图片里没有文字。请发带世界语或汉语文字的、清晰度尽量高的图片。 :)",
 								message, response);
 						return;
@@ -124,21 +131,25 @@ public class RequestServlet extends HttpServlet {
 						vortaroResultsString = resultsToString(vortaroResults);
 					else
 						vortaroResultsString = "【Hura】\nMi ne komprenas ĉi tiun tekston.\n抱歉，我看不懂这些文字，无法提供解释或翻译。 :(";
-					writeResponse(wrapOcrTextToResponse(text) + vortaroResultsString + ocrTip(), message, response);
+					writeResponse(SUCC, startTime, wrapOcrTextToResponse(text) + vortaroResultsString + ocrTip(),
+							message, response);
 					return;
 				}
 			default:
-//				writeResponse("Mi nur komprenas tekstojn kaj bildojn.\nHura目前只认识文字和图片消息哦，给我发文字，或者试试发图片吧。 :)", message, response);
-				writeResponse("Mi nur komprenas tekstojn.\nHura目前只认识文字消息哦，给我发文字吧。（图片文字识别功能即将开启，敬请期待） :)", message, response);
+				// writeResponse("Mi nur komprenas tekstojn kaj
+				// bildojn.\nHura目前只认识文字和图片消息哦，给我发文字，或者试试发图片吧。 :)", message, response);
+				writeResponse(NOSU, startTime,
+						"Mi nur komprenas tekstojn.\nHura目前只认识文字消息哦，给我发文字吧。（图片文字识别功能即将开启，敬请期待） :)", message, response);
 				return;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			writeResponse("Mi bedaŭras, sistemeraro estas okazinta.\n抱歉，我暂时出了点问题。请等会儿再来吧。 :(", message, response);
+			writeResponse(SERR, startTime, "Mi bedaŭras, sistemeraro estas okazinta.\n抱歉，我暂时出了点问题。请等会儿再来吧。 :(", message,
+					response);
 			return;
 		}
 	}
-	
+
 	private Message parseMessage(HttpServletRequest request) throws IOException {
 		request.setCharacterEncoding("UTF-8");
 
@@ -150,7 +161,6 @@ public class RequestServlet extends HttpServlet {
 			}
 		}
 		String str = strWriter.toString();
-		System.out.println("[REQUEST]" + str);
 		if (str.isEmpty()) {
 			return null;
 		}
@@ -201,10 +211,11 @@ public class RequestServlet extends HttpServlet {
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		System.out.println(resultsToString(new Vortaro().query("吃", QUERY_TIMEOUT)));
+		System.out.println("abc\ndef\r\nghi jkl\tmn".replaceAll("[\\r\\n\\s]", " "));
 	}
 
-	private void writeResponse(String resContent, Message reqMessage, HttpServletResponse response) throws IOException {
+	private void writeResponse(ResponseStatus status, long startTime, String resContent, Message reqMessage,
+			HttpServletResponse response) throws IOException {
 		Message resMessage = new Message();
 		resMessage.setFromUserName(reqMessage.getToUserName());
 		resMessage.setToUserName(reqMessage.getFromUserName());
@@ -218,11 +229,42 @@ public class RequestServlet extends HttpServlet {
 		writer.print(resString);
 		writer.flush();
 
-		System.out.println("[RESPONSE]" + resString);
+		log(status, startTime, reqMessage, resContent);
 	}
 
-	private void noResponse(HttpServletResponse response) {
-		System.out.println("[NO_RESPONSE]");
+	private void noResponse(Message reqMessage, HttpServletResponse response) {
+		log(NULL, null, reqMessage, null);
+	}
+
+	private void log(ResponseStatus status, Long startTime, Message reqMessage, String resContent) {
+		// status cost fromuser type req res
+		Logger.info(() -> {
+			long cost = startTime == null ? -1 : System.currentTimeMillis() - startTime;
+			List<String> infos = new ArrayList<>();
+
+			infos.add(status.name());
+			infos.add(Long.toString(cost));
+			infos.add(reqMessage == null ? "null" : reqMessage.getFromUserName());
+			infos.add(reqMessage == null ? "null" : reqMessage.getMsgType());
+			String reqContent;
+			switch (reqMessage == null ? "null" : reqMessage.getMsgType()) {
+			case "event":
+				reqContent = reqMessage.getEvent();
+				break;
+			case "text":
+				reqContent = reqMessage.getContent();
+				break;
+			case "image":
+				reqContent = reqMessage.getPicUrl();
+				break;
+			default:
+				reqContent = "null";
+			}
+			infos.add(reqContent.replaceAll("[\\r\\n\\s]", " "));
+			infos.add(resContent == null ? "null" : resContent.replaceAll("[\\r\\n\\s]", " "));
+
+			return String.join("\t", infos);
+		});
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
---
+++ src/main/java/com/github/blovemaple/hura/ResponseStatus.java
@@ -0,0 +1,8 @@
+package com.github.blovemaple.hura;
+
+/**
+ * @author blovemaple <blovemaple2010(at)gmail.com>
+ */
+public enum ResponseStatus {
+	SUCC, FAIL, SERR, NULL, NOSU
+}
